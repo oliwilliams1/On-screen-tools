e1f110783d32b3121c381583e1cbb09cb39004e3
oliwilliams1 <imhungryoliver@gmail.com>
2024-07-10 22:53:48 +1200

    Structure in App class

--- screenToolss/App.h
+++ screenToolss/App.h
@@ -17,12 +17,17 @@
 class App
 {
 public:
-    static App* instance;
+	// Dumb static thing but kinda cool
+	static App* instance;
+
+    App(int* argc, char** argv);
+	void run();
 
 private:
-
+	// Dynamic object array
 	std::vector<std::unique_ptr<baseObject>> objects;
 
+	// Member variables
 	float currentTime = 0.0f;
 	float previousTime = 0.0f;
 	bool debug = false;
@@ -34,178 +39,20 @@ private:
 	bool mouseDown;
 	vec2 widthHeight = vec2(windowSize.x / windowSize.x, windowSize.y / windowSize.y);
 
-    vec2 toNDC(vec2 value)
-    {
-        return vec2(value.x * 2 - 1, value.y * 2 - 1);
-    }
+	// Helper functions
+	vec2 toNDC(vec2 value);
+	const char* loadShaderSource(const char* filename);
 
-    static void mousePosCallback(int x, int y)
-    {
-        // Get mouse coords between 0 to 1
-        instance->mousePos = vec2((x / instance->windowSize.x), (y / instance->windowSize.y));
+	// Stupid callback functions
+	static void mousePosCallback(int x, int y);
+	static void renderCB();
+	static void mouseCB(int button, int state, int x, int y);
 
-        // To NDC
-        instance->mousePos = instance->toNDC(instance->mousePos);
+	// Easy way to add objects to vector
+	template <typename T>
+	void addObject(const T& object);
 
-        // Idk why it works but it does
-        instance->mousePos.y += 1.0f;
-    }
-
-    const char* loadShaderSource(const char* filename) {
-        std::ifstream file(filename, std::ios::in | std::ios::binary);
-        if (!file.is_open()) {
-            return nullptr;
-        }
-
-        file.seekg(0, std::ios::end);
-        std::streampos file_size = file.tellg();
-        file.seekg(0, std::ios::beg);
-
-        char* contents = new char[static_cast<size_t>(file_size) + 1];
-
-        file.read(contents, file_size);
-        contents[static_cast<size_t>(file_size)] = '\0';
-
-        file.close();
-        return contents;
-    }
-
-    static void renderCB()
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Calculate the delta time
-        instance->currentTime = static_cast<float>(glutGet(GLUT_ELAPSED_TIME)) / 1000.0f; // Elapsed time in seconds
-        float deltaTime = instance->currentTime - instance->previousTime;
-        instance->previousTime = instance->currentTime;
-
-        if (instance->mouseDown) {
-            instance->secondCorner = instance->mousePos - instance->firstCorner;
-        }
-
-        for (const auto& object : instance->objects) {
-            object->draw();
-        }
-
-        glutPostRedisplay();
-        glutSwapBuffers();
-    }
-
-    static void mouseCB(int button, int state, int x, int y)
-    {
-        if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-            instance->mouseDown = true;
-            instance->firstCorner = instance->mousePos;
-            instance->secondCorner = vec2(0);
-        }
-
-        if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
-            instance->mouseDown = false;
-        }
-    }
-
-    template <typename T>
-    void addObject(const T& object)
-    {
-        static_assert(std::is_base_of_v<baseObject, std::decay_t<T>>, "Object must inherit from basicObject");
-        objects.emplace_back(std::make_unique<std::decay_t<T>>(object));
-    }
-
-    void initScene(std::vector<uint8_t> imageData)
-    {
-        const vec2 rectVertices[] = {
-            {-0.5f, -0.5f},
-            {-0.5f,  0.5f},
-            { 0.5f,  0.5f},
-            { 0.5f, -0.5f}
-        };
-        const GLuint rectIndices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        const vec2 rectUVs[] = {
-            {0.0f, 0.0f},
-            {0.0f, 1.0f},
-            {1.0f, 1.0f},
-            {1.0f, 0.0f}
-        };
-
-        const char* rectVertexShaderSource = loadShaderSource("vert.glsl");
-        const char* rectFragmentShaderSource = loadShaderSource("frag.glsl");
-
-        const vec2 fullRectVertices[] = {
-            {-1.0f, -1.0f},
-            {-1.0f,  1.0f},
-            { 1.0f,  1.0f},
-            { 1.0f, -1.0f}
-        };
-
-        const char* fullRectVertexShaderSource = loadShaderSource("vert.glsl");
-        const char* fullRectFragmentShaderSource = loadShaderSource("frag2.glsl");
-
-        advancedObject screenObjects(fullRectVertices, 4, rectIndices, 6, rectUVs, 4, fullRectVertexShaderSource, fullRectFragmentShaderSource, imageData, imageType(1920, 1080));
-        addObject(screenObjects);
-
-        fractalRect fractalRectObject(rectVertices, 4, rectIndices, 6, rectUVs, 4, rectVertexShaderSource, rectFragmentShaderSource, &currentTime);
-        addObject(fractalRectObject);
-
-        const vec2 smallRectVertices[] = {
-            { 0.0f,  0.0f},
-            { 0.0f,  0.1f},
-            { 0.1f,  0.1f},
-            { 0.1f,  0.0f}
-        };
-
-        const char* smallRectVertexShaderSource = loadShaderSource("vert2.glsl");
-        const char* smallRectFragmentShaderSource = loadShaderSource("frag3.glsl");
-
-        const vec2 drawRectVertices[] = {
-            { 0.0f,  0.0f},
-            { 0.0f, -1.0f},
-            { 1.0f, -1.0f},
-            { 1.0f,  0.0f}
-        };
-
-        drawRect smallRect(drawRectVertices, 4, rectIndices, 6, rectUVs, 4, smallRectVertexShaderSource, smallRectFragmentShaderSource, &firstCorner, &secondCorner);
-        addObject(smallRect);
-    }
-
-public:
-    App(int *argc, char** argv)
-    {
-        if (instance == nullptr) {
-            instance = this;
-        }
-        ScreenshotTaker screenshotTaker;
-        std::vector<uint8_t> imageData = screenshotTaker.takeScreenshot();
-
-        glutInit(argc, argv);
-        if (debug) {
-            glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-        }
-        else {
-            glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_BORDERLESS);
-        }
-
-        glutInitWindowSize(windowSize.x, windowSize.y);
-        glutInitWindowPosition(0, 0);
-        glutCreateWindow("OpenGL Example");
-
-        glutPassiveMotionFunc(mousePosCallback);
-        glutMotionFunc(mousePosCallback);
-
-        glewInit();
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        initScene(imageData);
-    }
-
-    void run()
-    {
-        glutDisplayFunc(renderCB);
-        glutMouseFunc(mouseCB);
-        glutMainLoop();
-    }
+	// Init all objects for rendering
+	void initScene(std::vector<uint8_t> imageData);
 };
 
